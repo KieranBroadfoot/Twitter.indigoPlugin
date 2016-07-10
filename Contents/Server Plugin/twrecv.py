7e341e1ab869628ae1ef046acfd680cb02277fc0
kieran <kjb@l1fe.co.uk>
2016-07-10 16:45:40 +0100

    Improvements to twrecv.py to better handle network failures

--- Contents/Server Plugin/twrecv.py
+++ Contents/Server Plugin/twrecv.py
@@ -3,6 +3,7 @@
 import os
 import socket
 import simplejson as json
+import time
 from twitter import *
 
 APP_CREDS = os.path.expanduser('~/.twitter_app_credentials')
@@ -12,27 +13,32 @@ CONSUMER_KEY, CONSUMER_SECRET = read_token_file(APP_CREDS)
 oauth_token, oauth_secret = read_token_file(MY_CREDS)
 
 auth = OAuth(
-    consumer_key=CONSUMER_KEY,
-    consumer_secret=CONSUMER_SECRET,
-    token=oauth_token,
-    token_secret=oauth_secret
+	consumer_key=CONSUMER_KEY,
+	consumer_secret=CONSUMER_SECRET,
+	token=oauth_token,
+	token_secret=oauth_secret
 )
 
 twitter = Twitter(auth=auth)
 
 MY_HANDLE = twitter.account.verify_credentials()['screen_name']
-
-twitter_userstream = TwitterStream(auth=auth, domain='userstream.twitter.com')
-for msg in twitter_userstream.user():
-    if 'direct_message' in msg:
-        if msg['direct_message']['sender_screen_name'] != MY_HANDLE:
-            # not my own message.
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host = "127.0.0.1"
-            port = 13939
-            s.connect((host,port))
-            s.send(json.dumps({'handle': msg['direct_message']['sender_screen_name'], 
-                'text': msg['direct_message']['text'], 
-                'id': msg['direct_message']['id'],
-                'type': 'dm'}).encode()) 
-            s.close ()
+while True:
+	try:
+		twitter_userstream = TwitterStream(auth=auth, domain='userstream.twitter.com')
+		for msg in twitter_userstream.user():
+				if 'direct_message' in msg:
+					if msg['direct_message']['sender_screen_name'] != MY_HANDLE:
+							# not my own message.
+							s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+							host = "127.0.0.1"
+							port = 13939
+							s.connect((host,port))
+							s.send(json.dumps({'handle': msg['direct_message']['sender_screen_name'], 
+								'text': msg['direct_message']['text'], 
+								'id': msg['direct_message']['id'],
+								'type': 'dm'}).encode()) 
+							s.close ()
+	except Exception, e:
+		print str(e)
+	print "Lost connection.. Restarting in 5 seconds"
+	time.sleep(5)
